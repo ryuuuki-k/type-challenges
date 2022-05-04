6589e112632a1ffb67dadf350266004ab9a7b622
ryuki.katayama <ryuki.katayama@micro-wave.net>
2022-05-04 12:58:25 +0900

    add TupleToUnion.ts

---
+++ medium/TupleToUnion.ts
@@ -0,0 +1,7 @@
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00010-medium-tuple-to-union/README.md
+
+type TupleToUnion<T extends any[]> = T[number];
+
+type Arr = ['1', '2', '3'];
+
+type Test = TupleToUnion<Arr>; // expected to be '1' | '2' | '3'
